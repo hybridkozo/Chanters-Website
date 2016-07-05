4a0b85a697c5b7f23f37dc95160af6a8484ea550
hybridkozo <g.kozompolis@gmail.com>
2016-07-05 19:31:37 +0300

    Change menu to dynamic insert data from database

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=utf8');
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -28,7 +31,7 @@
 						</button>			
 					</div>
 					<div class="collapse navbar-collapse"  id="myNavbar">
-						<ul class="nav navbar-nav">
+						<!--<ul class="nav navbar-nav">
 							<li class="active"><a href="#">Αρχική</a></li>
 							<li class="dropdown">
 								<a class="dropdown-toggle" data-toggle="dropdown" href="#">Ο Σύλογος μας <span class="caret"></span></a>
@@ -41,6 +44,75 @@
 							<li><a href="#">Τα νέα μας</a></li>
 							<li><a href="#">Επικοινωνία</a></li>
 						</ul>
+						-->
+						
+						
+							
+				<?php
+				
+				$dbhost = 'localhost:3306';
+				$dbuser = 'john';
+				$dbpass = 'giannis';
+				$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+				if(!$conn){
+					die('could not connect: ' . mysql_error());
+				}
+				
+				$sql='SELECT * FROM menu ORDER by sequence;';
+				
+				mysql_select_db('chanters');
+				mysql_query("SET NAMES 'utf8'");
+				$retval = mysql_query( $sql, $conn );
+				if(! $retval )
+				{
+					die('Could not get data: ' . mysql_error());
+				}
+				$i=0;
+				echo '<ul class="nav navbar-nav">';
+				while($row = mysql_fetch_array($retval, MYSQL_ASSOC)){
+					if ($i==0 and $row['submenu']==NULL){
+						echo '<li class="active"><a href="#">' . $row['name'] . '</a></li>';
+						
+					}
+					else if ($i>0 and $row['submenu']==NULL){
+					echo '<li><a href="#">' . $row['name'] . '</a></li>';
+					}
+					else
+					{
+						$id=$row['idmenu'];
+						$sql2="SELECT * FROM submenu WHERE idmenu='$id' ORDER BY sequence;" ;
+						$retval2 = mysql_query( $sql2, $conn );
+						if(! $retval2 )
+						{
+							die('Could not get data: ' . mysql_error());
+						}
+						if ($i==0){
+						echo '<li class="dropdown, active"><a class="dropdown-toggle" data-toggle="dropdown" href="#">' . $row['name'] . '<span class="caret"></span></a>';
+						}
+						else{
+							echo '<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">' . $row['name'] . '<span class="caret"></span></a>';
+						}
+						echo '<ul class="dropdown-menu">';
+						while($row2 = mysql_fetch_array($retval2, MYSQL_ASSOC)){
+							echo '<li><a href="#">' . $row2['name'] . '</a></li>';
+						}
+						echo "</ul>";
+						echo "</li>";
+						
+					}
+					
+					$i++;
+				}
+				echo "</ul>";
+				
+				mysql_close($conn);
+				
+			?>
+						
+						
+						
+						
+						
 						<ul class="nav navbar-nav navbar-right">
 							<li><a href="#" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-user"></span> Εγγραφή</a>
 						<!-- Modal -->
@@ -143,18 +215,7 @@
 							</div>
 					</div>
 			</nav>
-			<?php
-				$dbhost = 'localhost:3306';
-				$dbuser = 'john';
-				$dbpass = 'giannis';
-				$conn = mysql_connect($dbhost, $dbuser, $dbpass);
-				if(!$conn){
-					die('could not connect: ' . mysql_error());
-				}
-				echo 'Connected to the database successfully';
-				mysql_close($conn);
-				
-			?>
+		
 		<div class="row">
 			<div class="col-md-9">
 			<h1> articles </h1>
